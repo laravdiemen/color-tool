ae452decdc506c15993fa3b93b850c1d0d1c02d1
Lara van Diemen <lara@yard.nl>
2024-12-24 11:34:49 +0100

    (feat): create seperate colorpalette component

--- app/page.js
+++ app/page.js
@@ -1,28 +1,9 @@
-import { generateColorPalette } from "@/app/_lib/colors";
+import ColorPalette from "@/app/_components/ColorPalette";
 
 export default function Page() {
-  const colorPalette = generateColorPalette("#14b8a6");
-
   return (
     <div>
-      <div className="grid grid-cols-4 gap-4">
-        <span>Key</span>
-        <span>3.0</span>
-        <span>4.5</span>
-        <span>7.0</span>
-      </div>
-      {Object.entries(colorPalette).map(([key, value]) => (
-        <div
-          key={key}
-          style={{ backgroundColor: value.color, color: value.contrast3 }}
-          className="grid grid-cols-4 gap-4"
-        >
-          <span>{key}</span>
-          <span style={{ color: value.contrast3 }}>{value.color}</span>
-          <span style={{ color: value.contrast45 }}>{value.color}</span>
-          <span style={{ color: value.contrast7 }}>{value.color}</span>
-        </div>
-      ))}
+      <ColorPalette color="#14b8a6" />
     </div>
   );
 }
