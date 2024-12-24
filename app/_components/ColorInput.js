aa8411b44913eff6899b16298b479add97287329
Lara van Diemen <lara@yard.nl>
2024-12-24 12:09:35 +0100

    (feat): use search params to save selected color

--- app/_components/ColorInput.js
+++ app/_components/ColorInput.js
@@ -1,15 +1,30 @@
 "use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useSettings } from "@/app/_contexts/SettingsContext";
+import { useEffect } from "react";
 
 export default function ColorInput() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
   const { inputColor, setInputColor } = useSettings();
 
-  return (
-    <input
-      type="color"
-      value={inputColor}
-      onChange={(e) => setInputColor(e.target.value)}
-    />
-  );
+  useEffect(() => {
+    const activeColor = searchParams.get("color") ?? "";
+
+    if (activeColor) {
+      setInputColor(activeColor);
+    }
+  }, [searchParams, setInputColor]);
+
+  function handleColorInput(e) {
+    const selectedColor = e.target.value;
+    setInputColor(selectedColor);
+    const params = new URLSearchParams(searchParams);
+    params.set("color", selectedColor);
+    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
+  }
+
+  return <input type="color" value={inputColor} onChange={handleColorInput} />;
 }
