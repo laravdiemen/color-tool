864079ce557b841aebb7e34acdea39ade4f9c6c7
Lara van Diemen <lara@yard.nl>
2024-12-31 14:48:22 +0100

    (style): change toast colors

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,23 @@ export default function RootLayout({ children }) {
         <main className="px-4 py-6 md:py-8 max-w-screen-md xl:max-w-screen-xl mx-auto flex flex-col gap-4">
           <SettingsProvider>{children}</SettingsProvider>
 
-          <Toaster position="top-right" />
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              success: {
+                iconTheme: {
+                  primary: "#15803d", // Tailwind green 700
+                  secondary: "#FFF",
+                },
+              },
+              error: {
+                iconTheme: {
+                  primary: "#dc2626", // Tailwind red 600
+                  secondary: "#FFF",
+                },
+              },
+            }}
+          />
         </main>
       </body>
     </html>
