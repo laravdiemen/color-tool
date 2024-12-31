703df4c66b0984671717e3ba55695a8cbcab550f
Lara van Diemen <lara@yard.nl>
2024-12-31 15:01:57 +0100

    (feat): add icon to color input

--- app/_components/ColorInput.js
+++ app/_components/ColorInput.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useSettings } from "@/app/_contexts/SettingsContext";
+import { EyeDropperIcon } from "@heroicons/react/24/outline";
 
 export default function ColorInput() {
   const { baseColor, updateBaseColor } = useSettings();
@@ -11,5 +12,23 @@ export default function ColorInput() {
   }
 
   // TODO: Add a text input as well with validation for hex colors
-  return <input type="color" value={baseColor} onChange={handleColorInput} />;
+  return (
+    <>
+      <div>
+        <label htmlFor="color-picker" className="sr-only">
+          Color picker
+        </label>
+        <div className="py-2 px-3 border border-slate-400 rounded relative transition hover:bg-slate-400">
+          <EyeDropperIcon className="size-4 pointer-events-none" />
+          <input
+            id="color-picker"
+            type="color"
+            value={baseColor}
+            onChange={handleColorInput}
+            className="opacity-0 absolute inset-0 size-full"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
