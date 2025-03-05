3fd73aa35461e284ec882af4b6212ca5aeda615c
Lara van Diemen <lara@yard.nl>
2025-03-05 16:23:17 +0100

    (a11y): only show outline if the user is tabbing

--- app/_lib/constants.js
+++ app/_lib/constants.js
@@ -1,1 +1,3 @@
 export const POSSIBLE_CONTRAST_RATIO = ["3.0", "4.5", "7.0"];
+
+export const BODY_CLASS_TABBING = "js-user-is-tabbing";
--- app/page.js
+++ app/page.js
@@ -1,10 +1,33 @@
+"use client";
+
+import { useEffect } from "react";
 import AccessibleColorAlternatives from "@/app/_components/AccessibleColorAlternatives";
 import ContrastRatioPalette from "@/app/_components/ContrastRatioPalette";
 import ColorInput from "@/app/_components/ColorInput";
 import TailwindColorPalette from "@/app/_components/TailwindColorPalette";
 import Heading from "@/app/_ui/Heading";
+import { BODY_CLASS_TABBING } from "@/app/_lib/constants";
 
 export default function Page() {
+  useEffect(() => {
+    const handleTab = (e) => {
+      if (e.key !== "Tab") return;
+      document.body.classList.add(BODY_CLASS_TABBING);
+    };
+
+    const handleMouseDown = () => {
+      document.body.classList.remove(BODY_CLASS_TABBING);
+    };
+
+    window.addEventListener("keydown", handleTab);
+    window.addEventListener("mousedown", handleMouseDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleTab);
+      window.removeEventListener("mousedown", handleMouseDown);
+    };
+  }, []);
+
   return (
     <>
       <Heading as="h1">Color Tool</Heading>
