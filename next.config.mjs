27bb6bb01a2da773984c88aa637175f8731715ec
Lara van Diemen <lara@yard.nl>
2025-03-12 12:25:54 +0100

    (feat): setup deploy to github-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  trailingSlash: true, // Ensures correct routing
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/color-tool/" : "",
+  basePath: isProd ? "/color-tool" : "",
+  output: "export",
+};
 
 export default nextConfig;
